9c686c199a5f9966953551213ed4aa3e24a88dc5
Nils <nilscrusell@gmail.com>
2026-04-16 17:10:41 +0200

    task5 och 5.1 Godkänd

---
+++ Data/static/task5-1.js
@@ -0,0 +1,63 @@
+document.getElementById("t5-1").addEventListener("click", function () {
+  map.flyTo([59.3609882, 18.0576769], 10);
+
+  /*
+  function sweden() {
+          if (mymap.hasLayer(swedishlayer)) {
+            mymap.removeLayer(swedishlayer);
+
+            sidebar.hide();
+          } else {
+            swedishlayer.addTo(mymap);
+
+            sidebar.show();
+          }
+        }
+  */
+
+  if (map.hasLayer(fueldonutCluster)) {
+    // samma logik som swedishlayer
+    map.removeLayer(fueldonutCluster);
+  } else {
+    fueldonutCluster.addTo(map);
+  }
+});
+
+// skapa en donut cluster grupp
+var fueldonutCluster = L.DonutCluster(
+  //inställningar
+  {
+    chunkedLoading: true,
+  },
+  {
+    // 'key' talar om för pluginet vilken egenskap i din data som ska användas för gruppering.
+    // I din fuel.js ligger märket under egenskapen "brand".
+    key: "brand",
+
+    // 'arcColorDict' definierar färgerna för varje kategori.
+    arcColorDict: {
+      Shell: "#FECD00",
+      "Circle K": "#EE342A",
+      OKQ8: "#2C3B8E",
+      St1: "#FEC700",
+      ST1: "#FEC700",
+      Preem: "#086746",
+      Ingo: "#FF7408",
+      Tanka: "#19438F",
+      IDS: "#FEFEFE",
+      Övriga: "#808080",
+    },
+  },
+);
+
+var fuelDonutLayer = L.geoJSON(fuel); // skapa en geojson layer med datan från fuel.js, så att varje punkt i datan blir en marker på kartan
+
+fuelDonutLayer.eachLayer(function (layer) {
+  // loopar igenom varje lager i fuelLayer
+  layer.on("click", function () {
+    var popupContent = "<h3>" + layer.feature.properties.name + "</h3>";
+    layer.bindPopup(popupContent).openPopup(); // först binder vi en popup till lagret, och sedan öppnas den direkt när lagret klickas på
+  });
+});
+
+fueldonutCluster.addLayer(fuelDonutLayer);
--- Data/static/task5.js
+++ Data/static/task5.js
@@ -1,6 +1,23 @@
 document.getElementById("t5").addEventListener("click", function () {
   map.flyTo([59.3609882, 18.0576769], 10);
+
+  if (map.hasLayer(fuelCluster)) {
+    map.removeLayer(fuelCluster);
+  } else {
+    fuelCluster.addTo(map);
+  }
 });
 
-var fuel = L.geoJson(fuel);
-fuel.addTo(map);
+var fuelCluster = L.markerClusterGroup(); // skapa en marker cluster grupp
+
+var fuelLayer = L.geoJSON(fuel); // skapa en geojson layer med datan från fuel.js, så att varje punkt i datan blir en marker på kartan
+
+fuelCluster.addLayer(fuelLayer); // lägg till fuelLayer i fuelCluster, så att alla markrar i fuelLayer kommer att klustras ihop i fuelCluster
+
+fuelLayer.eachLayer(function (layer) {
+  // loopar igenom varje lager i fuelLayer
+  layer.on("click", function () {
+    var popupContent = "<h3>" + layer.feature.properties.name + "</h3>";
+    layer.bindPopup(popupContent).openPopup(); // först binder en popup till lagret, och sedan öppnas den direkt när lagret klickas på
+  });
+});
