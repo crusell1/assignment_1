8c74708f43ab9bc5b6a1e5c7c79b2a16b3b35fad
Melinda Bodare <h24melbo@du.se>
2026-04-27 17:11:41 +0200

    kommenterat main.py

--- Data/main.py
+++ Data/main.py
@@ -1,10 +1,16 @@
+# Importerar flask
 from flask import Flask, render_template
 
+# Skapar webbapplikationen i variabeln "app"
 app = Flask(__name__)
 
+# Skapar en rutt till webbsidans startsida
 @app.route("/")
+# Definierar vad som ska köras vid start
 def home():
+    # Hämtar och skickar html-filen
     return render_template("index.html")
 
+# Startar wbbservern och aktiverar debug för felmeddelande
 if __name__ == "__main__":
     app.run(debug=True)
