d61538a30aad3a20a89fc10c9eef70800578750e
Nils <nilscrusell@gmail.com>
2026-04-28 13:50:07 +0200

    kommentar

--- Data/static/task5-1.js
+++ Data/static/task5-1.js
@@ -1,8 +1,8 @@
-document.getElementById('t5-1').addEventListener('click', function () {
+document.getElementById("t5-1").addEventListener("click", function () {
   map.flyTo([59.3609882, 18.0576769], 10);
 
   if (map.hasLayer(fueldonutCluster)) {
-    // samma logik som swedishlayer
+    // samma logik som task 5
     map.removeLayer(fueldonutCluster);
   } else {
     fueldonutCluster.addTo(map);
@@ -17,20 +17,20 @@ var fueldonutCluster = L.DonutCluster(
   },
   {
     // 'key' talar om för pluginet vilken egenskap i din data som ska användas för gruppering.
-    key: 'brand',
+    key: "brand",
 
     // 'arcColorDict' definierar färgerna för varje kategori.
     arcColorDict: {
-      Shell: '#FECD00',
-      'Circle K': '#EE342A',
-      OKQ8: '#2C3B8E',
-      St1: '#FEC700',
-      ST1: '#FEC700',
-      Preem: '#086746',
-      Ingo: '#FF7408',
-      Tanka: '#19438F',
-      IDS: '#FEFEFE',
-      Övriga: '#808080',
+      Shell: "#FECD00",
+      "Circle K": "#EE342A",
+      OKQ8: "#2C3B8E",
+      St1: "#FEC700",
+      ST1: "#FEC700",
+      Preem: "#086746",
+      Ingo: "#FF7408",
+      Tanka: "#19438F",
+      IDS: "#FEFEFE",
+      Övriga: "#808080",
     },
   },
 );
@@ -39,8 +39,8 @@ var fuelDonutLayer = L.geoJSON(fuel); // skapa en geojson layer med datan från 
 
 fuelDonutLayer.eachLayer(function (layer) {
   // loopar igenom varje lager i fuelLayer
-  layer.on('click', function () {
-    var popupContent = '<h3>' + layer.feature.properties.name + '</h3>';
+  layer.on("click", function () {
+    var popupContent = "<h3>" + layer.feature.properties.name + "</h3>";
     layer.bindPopup(popupContent).openPopup(); // först binder vi en popup till lagret, och sedan öppnas den direkt när lagret klickas på
   });
 });
